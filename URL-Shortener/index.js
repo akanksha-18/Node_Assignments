32a9cbf616e0f19b833ed8debb48ca0b2f8e371d
Akanksha Dubey <akankshadubey2558@gmail.com>
2024-07-16 21:46:48 +0530

    env

--- URL-Shortener/index.js
+++ URL-Shortener/index.js
@@ -25,18 +25,79 @@
 
 
 
+// const express = require("express");
+// const { connectToMongoDB } = require("./connection");
+// const urlRoute = require("./routes/url");
+// const URL = require("./models/url");
+// const shortid = require('shortid');
+// const cors = require('cors');
+
+// const app = express();
+// const PORT = 8001;
+
+// // connectToMongoDB('mongodb://localhost:27017/short-url')
+// connectToMongoDB('mongodb+srv://akankshadubey2558:gknklQe0cIkswWpU@cluster0.jacqlm1.mongodb.net/')
+//     .then(() => console.log("Mongoose connected"));
+
+// app.set('view engine', 'ejs');
+// app.set('views', './views');
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use("/url", urlRoute);
+
+// app.get('/', (req, res) => {
+//     res.render('index');
+// });
+
+// app.post('/url/shorten', async (req, res) => {
+//     const { originalUrl } = req.body;
+//     const shortId = shortid.generate();
+//     const shortUrl = `http://localhost:${PORT}/${shortId}`;
+    
+//     await URL.create({
+//         shortId: shortId,
+//         redirectURL: originalUrl,
+//         visitHistory: [],
+//     });
+
+//     res.json({ shortUrl });
+// });
+
+// app.get('/:shortId', async (req, res) => {
+//     const shortId = req.params.shortId;
+//     const entry = await URL.findOneAndUpdate(
+//         { shortId },
+//         {
+//             $push: {
+//                 visitHistory: {
+//                     timestamp: Date.now(),
+//                 }
+//             }
+//         }
+//     );
+//     if (entry) {
+//         res.redirect(entry.redirectURL);
+//     } else {
+//         res.status(404).send('Short URL not found');
+//     }
+// });
+
+// app.listen(PORT, () => console.log('Server started at PORT ' + PORT));
+
 const express = require("express");
 const { connectToMongoDB } = require("./connection");
 const urlRoute = require("./routes/url");
 const URL = require("./models/url");
 const shortid = require('shortid');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
-const PORT = 8001;
+const PORT = process.env.PORT || 8001;
 
-// connectToMongoDB('mongodb://localhost:27017/short-url')
-connectToMongoDB('mongodb+srv://akankshadubey2558:gknklQe0cIkswWpU@cluster0.jacqlm1.mongodb.net/')
+connectToMongoDB(process.env.MONGODB_URI)
     .then(() => console.log("Mongoose connected"));
 
 app.set('view engine', 'ejs');
@@ -54,7 +115,7 @@ app.get('/', (req, res) => {
 app.post('/url/shorten', async (req, res) => {
     const { originalUrl } = req.body;
     const shortId = shortid.generate();
-    const shortUrl = `http://localhost:${PORT}/${shortId}`;
+    const shortUrl = `${process.env.BASE_URL}/${shortId}`;
     
     await URL.create({
         shortId: shortId,
