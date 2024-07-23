93f17b47d740d6dd6b45f62d63a8b9325a7bca48
Akanksha Dubey <akankshadubey2558@gmail.com>
2024-07-23 19:27:33 +0530

    commit changes

--- Blog/app.js
+++ Blog/app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const Blog = require("./models/blog");
+const methodOverride = require('method-override');
 
 const userRoute = require("./routes/user");
 const blogRoute = require("./routes/blog");
@@ -24,6 +25,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthentiicationCookie("token"));
 app.use(express.static(path.resolve("./public")));
+app.use(methodOverride('_method'));
+app.use(methodOverride('_method'));
 
 app.get("/", async (req, res) => {
   const allBlogs = await Blog.find({});
--- Blog/routes/blog.js
+++ Blog/routes/blog.js
@@ -1,8 +1,66 @@
+// const { Router } = require("express");
+// const multer = require('multer');
+// const path = require('path');
+// const Blog = require('../models/blog');
+// const Comment=require('../models/comment');
+
+// const router = Router();
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, path.resolve(`./public/uploads/`));
+//     },
+//     filename: function (req, file, cb) {
+//         const fileName = `${Date.now()}-${file.originalname}`;
+//         cb(null, fileName);
+//     }
+// });
+
+// const upload = multer({ storage: storage });
+
+// router.get('/add-new', (req, res) => {
+//     return res.render('addBlog', {
+//         user: req.user,
+//     });
+// });
+// router.get('/:id',async(req,res)=>{
+//     const blog=await Blog.findById(req.params.id).populate('createdBy')
+//     const comments=await Comment.find({blogId:req.params.id}).populate('createdBy')
+//     console.log("cooments",comments);
+//     return res.render('blog',{
+//         user:req.user,
+//         blog,
+//         comments,
+//     })
+// })
+
+// router.post('/comment/:blogId',async(req,res)=>{
+//     await Comment.create({
+//         content:req.body.content,
+//         blogId:req.params.blogId,
+//         createdBy:req.user._id
+//     })
+//     return res.redirect(`/blog/${req.params.blogId}`);
+// })
+
+// router.post('/', upload.single('coverImage'), async (req, res) => {
+//     const { title, body } = req.body;
+//     const blog = await Blog.create({
+//         body,
+//         title,
+//         createdBy: req.user._id,
+//         coverImageURL: `/uploads/${req.file.filename}` // Corrected here
+//     });
+//     return res.redirect(`/blog/${blog._id}`);
+// });
+
+// module.exports = router;
+
 const { Router } = require("express");
 const multer = require('multer');
 const path = require('path');
 const Blog = require('../models/blog');
-const Comment=require('../models/comment');
+const Comment = require('../models/comment');
 
 const router = Router();
 
@@ -23,35 +81,71 @@ router.get('/add-new', (req, res) => {
         user: req.user,
     });
 });
-router.get('/:id',async(req,res)=>{
-    const blog=await Blog.findById(req.params.id).populate('createdBy')
-    const comments=await Comment.find({blogId:req.params.id}).populate('createdBy')
-    console.log("cooments",comments);
-    return res.render('blog',{
-        user:req.user,
-        blog,
-        comments,
-    })
-})
 
-router.post('/comment/:blogId',async(req,res)=>{
-    await Comment.create({
-        content:req.body.content,
-        blogId:req.params.blogId,
-        createdBy:req.user._id
-    })
-    return res.redirect(`/blog/${req.params.blogId}`);
-})
+router.get('/:id', async (req, res) => {
+    try {
+        const blog = await Blog.findById(req.params.id).populate('createdBy');
+        const comments = await Comment.find({ blogId: req.params.id }).populate('createdBy');
+        if (!blog) {
+            return res.status(404).send('Blog not found');
+        }
+        return res.render('blog', {
+            user: req.user,
+            blog,
+            comments,
+        });
+    } catch (error) {
+        return res.status(500).send('Server error');
+    }
+});
+
+router.post('/comment/:blogId', async (req, res) => {
+    try {
+        await Comment.create({
+            content: req.body.content,
+            blogId: req.params.blogId,
+            createdBy: req.user._id
+        });
+        return res.redirect(`/blog/${req.params.blogId}`);
+    } catch (error) {
+        return res.status(500).send('Server error');
+    }
+});
 
 router.post('/', upload.single('coverImage'), async (req, res) => {
-    const { title, body } = req.body;
-    const blog = await Blog.create({
-        body,
-        title,
-        createdBy: req.user._id,
-        coverImageURL: `/uploads/${req.file.filename}` // Corrected here
-    });
-    return res.redirect(`/blog/${blog._id}`);
+    try {
+        const { title, body } = req.body;
+        const blog = await Blog.create({
+            body,
+            title,
+            createdBy: req.user._id,
+            coverImageURL: `/uploads/${req.file.filename}`
+        });
+        return res.redirect(`/blog/${blog._id}`);
+    } catch (error) {
+        return res.status(500).send('Server error');
+    }
+});
+
+// DELETE route for blog
+router.delete('/:id', async (req, res) => {
+    try {
+        const blog = await Blog.findById(req.params.id);
+        if (!blog) {
+            return res.status(404).send('Blog not found');
+        }
+
+        if (blog.createdBy.toString() !== req.user._id.toString()) {
+            return res.status(403).send('You are not authorized to delete this blog');
+        }
+
+        await Blog.findByIdAndDelete(req.params.id);
+        await Comment.deleteMany({ blogId: req.params.id });
+        // return res.status(200).send('Blog deleted successfully');
+        return res.redirect('/');
+    } catch (error) {
+        return res.status(500).send('Server error');
+    }
 });
 
 module.exports = router;
