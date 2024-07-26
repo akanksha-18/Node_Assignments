2e50b318af5a48ff806edbf4c36d5c728bb27a98
akanksha-18 <76443240+akanksha-18@users.noreply.github.com>
2024-07-26 12:42:48 +0530

    Update ShopCategory.jsx

--- Full_Stack_Ecommerce/frontend/src/Pages/ShopCategory.jsx
+++ Full_Stack_Ecommerce/frontend/src/Pages/ShopCategory.jsx
@@ -9,7 +9,7 @@ const ShopCategory = (props) => {
   const [allproducts, setAllProducts] = useState([]);
 
   const fetchInfo = () => { 
-    fetch('http://localhost:4000/allproducts') 
+    fetch('https://ecommerce-backend-7iq8.onrender.com/allproducts') 
             .then((res) => res.json()) 
             .then((data) => setAllProducts(data))
     }
